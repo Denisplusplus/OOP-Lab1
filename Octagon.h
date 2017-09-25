5031673989a4f74de6e1fb810b3c0a1034bc9045
Denisplusplus <whinslow@mail.ru>
2017-09-25 10:36:54 +0300

    Create Octagon.h

---
+++ Octagon.h
@@ -0,0 +1,20 @@
+#ifndef OCTAGON_H
+#define OCTAGON_H
+#include <iostream>
+#include <cstdlib>
+#include "Figure.h"
+
+class Octagon : public Figure {
+public:
+	Octagon();
+	Octagon(std::istream &is);
+	Octagon(size_t i);
+	Octagon(const Octagon& orig);
+	double Square() override;
+	void Print() override;
+	virtual ~Octagon();
+private:
+	size_t side_a;
+};
+
+#endif /*OCTAGON_H */
